90bec00691991ff058cd85448ffe8efa8f5b3377
Nick Gravgaard <nick@beautifulcode.net>
2015-02-06 18:10:06 +0000

    Get record from address_base table of DB

--- service/__init__.py
+++ service/__init__.py
@@ -1,7 +1,11 @@
 import os, logging
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ.get('SETTINGS'))
+
+
+db = SQLAlchemy(app)
