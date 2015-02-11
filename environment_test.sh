81ec0de153d718905087863391b769d2ba898042
Leszek Gonczar <gonczar@gmail.com>
2015-02-11 16:29:46 +0000

    Extract PostgreSQL database URL to an environment property

--- environment_test.sh
+++ environment_test.sh
@@ -1,2 +1,3 @@
 export SETTINGS='config.TestConfig'
 export PPI_END_POINT='http://landregistry.data.gov.uk/landregistry/query'
+export POSTGRES_DATABASE_URI='postgresql+pg8000://postgres:seekritpwd@172.16.42.43:5432/pubdata'
