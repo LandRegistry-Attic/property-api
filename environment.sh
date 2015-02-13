a4de064c4fcf68e7b19e1d039a1e0a62e056b54f
Camille Baldock <camille@camillebaldock.co.uk>
2015-02-13 16:11:53 +0000

    Change default password to password

--- environment.sh
+++ environment.sh
@@ -1,3 +1,3 @@
 export SETTINGS='config.DevelopmentConfig'
 export PPI_END_POINT='http://landregistry.data.gov.uk/landregistry/query'
-export POSTGRES_DATABASE_URI='postgresql+pg8000://postgres:seekritpwd@172.16.42.43:5432/pubdata'
+export POSTGRES_DATABASE_URI='postgresql+pg8000://postgres:password@172.16.42.43:5432/pubdata'
