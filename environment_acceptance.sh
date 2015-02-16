14176c38d36a9c9717e8b371cd02803350ec31d2
Nick Gravgaard <nick@beautifulcode.net>
2015-02-16 12:05:43 +0000

    Switch to using Elastic Search instead of Postgres

--- environment_acceptance.sh
+++ environment_acceptance.sh
@@ -1,3 +1,3 @@
 export SETTINGS='config.TestConfig'
 export PPI_END_POINT='http://landregistry.local:8002/get_ppi'
-export POSTGRES_DATABASE_URI='postgresql+pg8000://postgres:seekritpwd@172.16.42.43:5432/pubdata'
+export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-77-238-128.eu-west-1.compute.amazonaws.com:4200'
