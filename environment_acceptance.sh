39ebc11d0ff4fb0ae30978dc62f2cd4dbfd7f81f
Nick Gravgaard <nick@beautifulcode.net>
2015-02-17 14:37:21 +0000

    Use port 4300 for acceptance tests

--- environment_acceptance.sh
+++ environment_acceptance.sh
@@ -1,3 +1,3 @@
 export SETTINGS='config.TestConfig'
 export PPI_END_POINT='http://landregistry.local:8002/get_ppi'
-export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-77-238-128.eu-west-1.compute.amazonaws.com:4200'
+export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-77-238-128.eu-west-1.compute.amazonaws.com:4300'
