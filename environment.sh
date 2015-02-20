82f5f1063da2cda1863d4a5166bfdeb2b4f21016
Nick Gravgaard <nick@beautifulcode.net>
2015-02-20 14:37:08 +0000

    Fix elasticsearch endpoints

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 export SETTINGS='config.DevelopmentConfig'
 export PPI_END_POINT='http://landregistry.data.gov.uk/landregistry/query'
-export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-72-123-155.eu-west-1.compute.amazonaws.com:4200'
+export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-154-72-110.eu-west-1.compute.amazonaws.com:4200'
 export LOGGING_CONFIG_FILE='property-api-log-config.json'
