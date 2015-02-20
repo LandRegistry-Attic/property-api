71833261aa6b6f7d9408bce4e90fec4f9f2d75c3
Leszek Gonczar <gonczar@gmail.com>
2015-02-20 10:57:08 +0000

    Setup logging

--- environment_acceptance.sh
+++ environment_acceptance.sh
@@ -1,3 +1,4 @@
 export SETTINGS='config.TestConfig'
 export PPI_END_POINT='http://landregistry.local:8002/get_ppi'
 export ELASTIC_SEARCH_ENDPOINT='http://ec2-54-72-123-155.eu-west-1.compute.amazonaws.com:4300'
+export LOGGING_CONFIG_FILE='property-api-log-config.json'
