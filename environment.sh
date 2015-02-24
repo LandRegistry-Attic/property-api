e8fcaa102dc1612c820e1fa569f3512d7624dc78
Nick Gravgaard <nick@beautifulcode.net>
2015-02-24 16:28:31 +0000

    Change environment variables to use DNS hostname

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 export SETTINGS='config.DevelopmentConfig'
 export PPI_END_POINT='http://landregistry.data.gov.uk/landregistry/query'
-export ELASTIC_SEARCH_ENDPOINT='http://54.154.72.110:4200'
+export ELASTIC_SEARCH_ENDPOINT='http://preview-elasticsearch-01.beta.landregistryconcept.co.uk:4200'
 export LOGGING_CONFIG_FILE='property-api-log-config.json'
--- environment_acceptance.sh
+++ environment_acceptance.sh
@@ -1,4 +1,4 @@
 export SETTINGS='config.TestConfig'
 export PPI_END_POINT='http://landregistry.local:8002/get_ppi'
-export ELASTIC_SEARCH_ENDPOINT='http://54.154.72.110:4300'
+export ELASTIC_SEARCH_ENDPOINT='http://preview-elasticsearch-01.beta.landregistryconcept.co.uk:4300'
 export LOGGING_CONFIG_FILE='property-api-log-config.json'
